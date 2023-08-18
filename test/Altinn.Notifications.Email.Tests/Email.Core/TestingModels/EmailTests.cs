9bedf60608f067097ebca6e9e21de8791b6249a2
acn-sbuad <stephanie.buadu@avanade.com>
2023-08-18 17:30:29 +0200

    added unit tests

---
+++ test/Altinn.Notifications.Email.Tests/Email.Core/TestingModels/EmailTests.cs
@@ -0,0 +1,57 @@
+﻿using System.Text.Json.Nodes;
+
+using Altinn.Notifications.Email.Core.Models;
+
+using Xunit;
+
+namespace Altinn.Notifications.Tests.Notifications.Core.TestingModels;
+public class EmailTests
+{
+    private readonly string _serializedEmail;
+
+    public EmailTests()
+    {
+        Guid id = Guid.NewGuid();
+        _serializedEmail = new JsonObject()
+        {
+            { "id", id },
+            { "subject", "subject" },
+            { "body", "body" },
+            { "fromAddress", "from@domain.com" },
+            { "toAddress", "to@domain.com" },
+            { "contentType", "Html" }
+        }.ToJsonString();
+    }
+
+    [Fact]
+    public void TryParse_ValidEmail_True()
+    {
+        bool actualResult = Email.Core.Models.Email.TryParse(_serializedEmail, out Email.Core.Models.Email actual);
+        Assert.True(actualResult);
+        Assert.Equal("subject", actual.Subject);
+        Assert.Equal(EmailContentType.Html, actual.ContentType);
+    }
+
+    [Fact]
+    public void TryParse_EmptyString_False()
+    {
+        bool actualResult = Email.Core.Models.Email.TryParse(string.Empty, out _);
+        Assert.False(actualResult);
+    }
+
+    [Fact]
+    public void TryParse_InvalidString_False()
+    {
+        bool actualResult = Email.Core.Models.Email.TryParse("{\"ticket\":\"noTicket\"}", out _);
+
+        Assert.False(actualResult);
+    }
+
+    [Fact]
+    public void TryParse_InvalidJsonExceptionThrown_False()
+    {
+        bool actualResult = Email.Core.Models.Email.TryParse("{\"ticket:\"noTicket\"}", out _);
+
+        Assert.False(actualResult);
+    }
+}
---
+++ test/Altinn.Notifications.Email.Tests/Email.Integrations/ServiceCollectionExtensionsTests.cs
@@ -0,0 +1,37 @@
+﻿using Altinn.Notifications.Email.Integrations.Configuration;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using Xunit;
+
+namespace Altinn.Notifications.Tests.Notifications.Integrations.TestingExtensions;
+
+public class ServiceCollectionExtensionsTests
+{
+    [Fact]
+    public void AddIntegrationServices_KafkaSettingsMissing_ThrowsException()
+    {
+        Environment.SetEnvironmentVariable("KafkaSettings__ConsumerGroupId", null);
+        Environment.SetEnvironmentVariable("CommunicationServicesSettings__Connectionstring", "value");
+
+        var config = new ConfigurationBuilder().AddEnvironmentVariables().Build();
+
+        IServiceCollection services = new ServiceCollection();
+
+        Assert.Throws<ArgumentNullException>(() => services.AddIntegrationServices(config));
+    }
+
+    [Fact]
+    public void AddIntegrationServicess_NotificationOrderConfigMissing_ThrowsException()
+    {
+        Environment.SetEnvironmentVariable("KafkaSettings__ConsumerGroupId", "value");
+        Environment.SetEnvironmentVariable("CommunicationServicesSettings__Connectionstring", null);
+
+        var config = new ConfigurationBuilder().AddEnvironmentVariables().Build();
+
+        IServiceCollection services = new ServiceCollection();
+
+        Assert.Throws<ArgumentNullException>(() => services.AddIntegrationServices(config));
+    }
+}

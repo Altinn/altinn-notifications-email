0bcbc36336f52adc1ddae49971895a18d6594d04
tba76 <thomabak@gmail.com>
2024-03-06 08:24:45 +0100

    Fixed EmailSendResultMapperTests

--- test/Altinn.Notifications.Email.Tests/Email.Core/TestingStatus/EmailSendResultMapperTests.cs
+++ test/Altinn.Notifications.Email.Tests/Email.Core/TestingStatus/EmailSendResultMapperTests.cs
@@ -7,13 +7,14 @@ namespace Altinn.Notifications.Email.Tests.Email.Core.TestingStatus;
 public class EmailSendResultMapperTests
 {
     [Theory]
+    [InlineData(null, EmailSendResult.Failed)]
     [InlineData("Bounced", EmailSendResult.Failed_Bounced)]
     [InlineData("Delivered", EmailSendResult.Delivered)]
     [InlineData("Failed", EmailSendResult.Failed)]
     [InlineData("FilteredSpam", EmailSendResult.Failed_FilteredSpam)]
     [InlineData("Quarantined", EmailSendResult.Failed_Quarantined)]
     [InlineData("Suppressed", EmailSendResult.Failed_SupressedRecipient)]
-    public void ParseDeliveryStatus_ReturnsCorrectResult(string deliveryStatus, EmailSendResult expectedResult)
+    public void ParseDeliveryStatus_ReturnsCorrectResult(string? deliveryStatus, EmailSendResult expectedResult)
     {
         var result = EmailSendResultMapper.ParseDeliveryStatus(deliveryStatus);
         Assert.Equal(expectedResult, result);

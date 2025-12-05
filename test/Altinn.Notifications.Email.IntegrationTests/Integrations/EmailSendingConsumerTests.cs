cc0308b263ae2da0a4e014f89d35702a89102a82
Ahmed-Ghanam <ahmed.m.ghanam@outlook.com>
2025-12-05 09:59:11 +0100

    Dispose objects when they are not needed.

--- test/Altinn.Notifications.Email.IntegrationTests/Integrations/EmailSendingConsumerTests.cs
+++ test/Altinn.Notifications.Email.IntegrationTests/Integrations/EmailSendingConsumerTests.cs
@@ -20,7 +20,6 @@ namespace Altinn.Notifications.Email.IntegrationTests.Integrations;
 
 public class EmailSendingConsumerTests : IAsyncLifetime
 {
-    private ServiceProvider? _serviceProvider;
     private readonly KafkaSettings _kafkaSettings;
 
     private readonly string _emailSendingConsumerTopic = Guid.NewGuid().ToString();
@@ -61,18 +60,16 @@ public class EmailSendingConsumerTests : IAsyncLifetime
         // Arrange
         var processedSignal = new ManualResetEventSlim(false);
         var sendingServiceMock = CreateSendingServiceMock(processedSignal);
-
-        using SendEmailQueueConsumer consumer = GetEmailSendingConsumer(sendingServiceMock.Object);
-        using CommonProducer producer = KafkaUtil.GetKafkaProducer(_serviceProvider!);
+        await using var testFixture = CreateTestFixture(sendingServiceMock.Object);
 
         var email = new Core.Sending.Email(Guid.NewGuid(), "test", "body", "fromAddress", "toAddress", EmailContentType.Plain);
 
         // Act
-        await consumer.StartAsync(CancellationToken.None);
-        await producer.ProduceAsync(_emailSendingConsumerTopic, JsonSerializer.Serialize(email));
+        await testFixture.Consumer.StartAsync(CancellationToken.None);
+        await testFixture.Producer.ProduceAsync(_emailSendingConsumerTopic, JsonSerializer.Serialize(email));
 
         bool processed = await WaitForConditionAsync(() => processedSignal.IsSet, TimeSpan.FromSeconds(5), TimeSpan.FromMilliseconds(50));
-        await consumer.StopAsync(CancellationToken.None);
+        await testFixture.Consumer.StopAsync(CancellationToken.None);
 
         // Assert
         Assert.True(processed, "Email was not processed within the expected time window.");
@@ -85,16 +82,14 @@ public class EmailSendingConsumerTests : IAsyncLifetime
         // Arrange
         var processedSignal = new ManualResetEventSlim(false);
         var sendingServiceMock = CreateSendingServiceMock(processedSignal);
-
-        using SendEmailQueueConsumer consumer = GetEmailSendingConsumer(sendingServiceMock.Object);
-        using CommonProducer producer = KafkaUtil.GetKafkaProducer(_serviceProvider!);
+        await using var testFixture = CreateTestFixture(sendingServiceMock.Object);
 
         // Act
-        await consumer.StartAsync(CancellationToken.None);
-        await producer.ProduceAsync(_emailSendingConsumerTopic, "Not an email");
+        await testFixture.Consumer.StartAsync(CancellationToken.None);
+        await testFixture.Producer.ProduceAsync(_emailSendingConsumerTopic, "Not an email");
 
         bool processed = await WaitForConditionAsync(() => processedSignal.IsSet, TimeSpan.FromSeconds(2), TimeSpan.FromMilliseconds(50));
-        await consumer.StopAsync(CancellationToken.None);
+        await testFixture.Consumer.StopAsync(CancellationToken.None);
 
         // Assert
         Assert.False(processed, "Service should not be called when deserialization fails.");
@@ -138,24 +133,23 @@ public class EmailSendingConsumerTests : IAsyncLifetime
                 }
             });
 
-        using SendEmailQueueConsumer consumer = GetEmailSendingConsumer(sendingServiceMock.Object);
-        using CommonProducer producer = KafkaUtil.GetKafkaProducer(_serviceProvider!);
+        await using var testFixture = CreateTestFixture(sendingServiceMock.Object);
 
         var emails = Enumerable.Range(0, 50)
             .Select(i => new Core.Sending.Email(Guid.NewGuid(), $"subject-{i}", $"body-{i}", "from", "to", EmailContentType.Plain))
             .ToList();
 
         // Act
-        await consumer.StartAsync(CancellationToken.None);
+        await testFixture.Consumer.StartAsync(CancellationToken.None);
 
         foreach (var email in emails)
         {
-            await producer.ProduceAsync(_emailSendingConsumerTopic, JsonSerializer.Serialize(email));
+            await testFixture.Producer.ProduceAsync(_emailSendingConsumerTopic, JsonSerializer.Serialize(email));
         }
 
         var isProcessed = await WaitForConditionAsync(() => allMessagesProcessedSignal.IsSet, TimeSpan.FromSeconds(15), TimeSpan.FromMilliseconds(25));
 
-        await consumer.StopAsync(CancellationToken.None);
+        await testFixture.Consumer.StopAsync(CancellationToken.None);
 
         // Assert
         Assert.True(isProcessed, "All messages were not processed within expected time.");
@@ -169,18 +163,16 @@ public class EmailSendingConsumerTests : IAsyncLifetime
         // Arrange
         var processedSignal = new ManualResetEventSlim(false);
         var sendingServiceMock = CreateSendingServiceMock(processedSignal);
-
-        using SendEmailQueueConsumer consumer = GetEmailSendingConsumer(sendingServiceMock.Object);
-        using CommonProducer producer = KafkaUtil.GetKafkaProducer(_serviceProvider!);
+        await using var testFixture = CreateTestFixture(sendingServiceMock.Object);
 
         var email = new Core.Sending.Email(Guid.NewGuid(), "test", "body", "fromAddress", "toAddress", EmailContentType.Plain);
 
         // Act
-        await consumer.StartAsync(CancellationToken.None);
-        await producer.ProduceAsync(_emailSendingConsumerTopic, JsonSerializer.Serialize(email));
+        await testFixture.Consumer.StartAsync(CancellationToken.None);
+        await testFixture.Producer.ProduceAsync(_emailSendingConsumerTopic, JsonSerializer.Serialize(email));
 
         bool processed = await WaitForConditionAsync(() => processedSignal.IsSet, TimeSpan.FromSeconds(5), TimeSpan.FromMilliseconds(50));
-        await consumer.StopAsync(CancellationToken.None);
+        await testFixture.Consumer.StopAsync(CancellationToken.None);
 
         // Assert
         Assert.True(processed, "Message produced to the configured topic was not consumed, implying missing subscription.");
@@ -192,20 +184,18 @@ public class EmailSendingConsumerTests : IAsyncLifetime
         // Arrange
         var processedSignal = new ManualResetEventSlim(false);
         var sendingServiceMock = CreateSendingServiceMock(processedSignal);
-
-        using SendEmailQueueConsumer consumer = GetEmailSendingConsumer(sendingServiceMock.Object);
-        using CommonProducer producer = KafkaUtil.GetKafkaProducer(_serviceProvider!);
+        await using var testFixture = CreateTestFixture(sendingServiceMock.Object);
 
         var email = new Core.Sending.Email(Guid.NewGuid(), "subject-1", "body-1", "from-1", "to-1", EmailContentType.Plain);
 
         // Act
-        await consumer.StartAsync(CancellationToken.None);
-        await producer.ProduceAsync(_emailSendingConsumerTopic, JsonSerializer.Serialize(email));
+        await testFixture.Consumer.StartAsync(CancellationToken.None);
+        await testFixture.Producer.ProduceAsync(_emailSendingConsumerTopic, JsonSerializer.Serialize(email));
         var isProcessed = await WaitForConditionAsync(() => processedSignal.IsSet, TimeSpan.FromSeconds(5), TimeSpan.FromMilliseconds(50));
 
         var stopwatch = Stopwatch.StartNew();
         using var stopTimeout = new CancellationTokenSource(TimeSpan.FromSeconds(5));
-        await consumer.StopAsync(stopTimeout.Token);
+        await testFixture.Consumer.StopAsync(stopTimeout.Token);
         stopwatch.Stop();
 
         // Assert
@@ -243,21 +233,20 @@ public class EmailSendingConsumerTests : IAsyncLifetime
             })
             .Returns(Task.CompletedTask);
 
-        using SendEmailQueueConsumer firstConsumer = GetEmailSendingConsumer(sendingServiceMock.Object, loggerMock.Object);
-        using CommonProducer producer = KafkaUtil.GetKafkaProducer(_serviceProvider!);
+        await using var firstTestFixture = CreateTestFixture(sendingServiceMock.Object);
 
         var firstEmail = new Core.Sending.Email(firstEmailNotificationIdentifer, "first", "body-1", "from", "to", EmailContentType.Plain);
         var secondEmail = new Core.Sending.Email(secondEmailNotificationIdentifer, "second", "body-2", "from", "to", EmailContentType.Plain);
 
         // Act
-        await firstConsumer.StartAsync(CancellationToken.None);
+        await firstTestFixture.Consumer.StartAsync(CancellationToken.None);
 
-        await producer.ProduceAsync(_emailSendingConsumerTopic, JsonSerializer.Serialize(firstEmail));
-        await producer.ProduceAsync(_emailSendingConsumerTopic, JsonSerializer.Serialize(secondEmail));
+        await firstTestFixture.Producer.ProduceAsync(_emailSendingConsumerTopic, JsonSerializer.Serialize(firstEmail));
+        await firstTestFixture.Producer.ProduceAsync(_emailSendingConsumerTopic, JsonSerializer.Serialize(secondEmail));
 
         var firstProcessed = await WaitForConditionAsync(() => firstProcessedSignal.IsSet, TimeSpan.FromSeconds(5), TimeSpan.FromMilliseconds(50));
 
-        await firstConsumer.StopAsync(CancellationToken.None);
+        await firstTestFixture.Consumer.StopAsync(CancellationToken.None);
 
         loggerMock.Verify(
            e => e.Log(
@@ -270,13 +259,13 @@ public class EmailSendingConsumerTests : IAsyncLifetime
 
         allowSecondProcessing.Release();
 
-        using SendEmailQueueConsumer secondConsumer = GetEmailSendingConsumer(sendingServiceMock.Object, loggerMock.Object);
+        await using var secondTestFixture = CreateTestFixture(sendingServiceMock.Object);
 
-        await secondConsumer.StartAsync(CancellationToken.None);
-        
+        await secondTestFixture.Consumer.StartAsync(CancellationToken.None);
+
         var secondProcessed = await WaitForConditionAsync(() => secondProcessedSignal.IsSet, TimeSpan.FromSeconds(5), TimeSpan.FromMilliseconds(50));
 
-        await secondConsumer.StopAsync(CancellationToken.None);
+        await secondTestFixture.Consumer.StopAsync(CancellationToken.None);
 
         // Assert
         Assert.True(firstProcessed, "First email was not processed within the expected time window.");
@@ -293,24 +282,22 @@ public class EmailSendingConsumerTests : IAsyncLifetime
         // Arrange
         var firstProcessedSignal = new ManualResetEventSlim(false);
         var sendingServiceMock = CreateSendingServiceMock(firstProcessedSignal);
-
-        using SendEmailQueueConsumer consumer = GetEmailSendingConsumer(sendingServiceMock.Object);
-        using CommonProducer producer = KafkaUtil.GetKafkaProducer(_serviceProvider!);
+        await using var testFixture = CreateTestFixture(sendingServiceMock.Object);
 
         var firstEmail = new Core.Sending.Email(Guid.NewGuid(), "first", "body-1", "from-1", "to-1", EmailContentType.Plain);
         var afterStopEmail = new Core.Sending.Email(Guid.NewGuid(), "after", "body-3", "from-3", "to-3", EmailContentType.Plain);
         var duringShutdownEmail = new Core.Sending.Email(Guid.NewGuid(), "during", "body-2", "from-2", "to-2", EmailContentType.Plain);
 
         // Act
-        await consumer.StartAsync(CancellationToken.None);
-        await producer.ProduceAsync(_emailSendingConsumerTopic, JsonSerializer.Serialize(firstEmail));
+        await testFixture.Consumer.StartAsync(CancellationToken.None);
+        await testFixture.Producer.ProduceAsync(_emailSendingConsumerTopic, JsonSerializer.Serialize(firstEmail));
         var isFirstProcessed = await WaitForConditionAsync(() => firstProcessedSignal.IsSet, TimeSpan.FromSeconds(5), TimeSpan.FromMilliseconds(50));
-     
-        var stopTask = consumer.StopAsync(CancellationToken.None);
-        await producer.ProduceAsync(_emailSendingConsumerTopic, JsonSerializer.Serialize(duringShutdownEmail));
+
+        var stopTask = testFixture.Consumer.StopAsync(CancellationToken.None);
+        await testFixture.Producer.ProduceAsync(_emailSendingConsumerTopic, JsonSerializer.Serialize(duringShutdownEmail));
         await stopTask;
 
-        await producer.ProduceAsync(_emailSendingConsumerTopic, JsonSerializer.Serialize(afterStopEmail));
+        await testFixture.Producer.ProduceAsync(_emailSendingConsumerTopic, JsonSerializer.Serialize(afterStopEmail));
         await Task.Delay(TimeSpan.FromSeconds(1));
 
         // Assert
@@ -346,8 +333,7 @@ public class EmailSendingConsumerTests : IAsyncLifetime
                 }
             });
 
-        using SendEmailQueueConsumer consumer = GetEmailSendingConsumer(sendingServiceMock.Object, loggerMock.Object);
-        using CommonProducer producer = KafkaUtil.GetKafkaProducer(_serviceProvider!);
+        await using var testFixture = CreateTestFixture(sendingServiceMock.Object);
 
         // Produce 150 messages; base max batch size is 100
         var emails = Enumerable.Range(0, 150)
@@ -355,25 +341,46 @@ public class EmailSendingConsumerTests : IAsyncLifetime
             .ToList();
 
         // Act
-        await consumer.StartAsync(CancellationToken.None);
+        await testFixture.Consumer.StartAsync(CancellationToken.None);
 
         foreach (var email in emails)
         {
-            await producer.ProduceAsync(_emailSendingConsumerTopic, JsonSerializer.Serialize(email));
+            await testFixture.Producer.ProduceAsync(_emailSendingConsumerTopic, JsonSerializer.Serialize(email));
         }
 
+        var processedFirst100 = await WaitForConditionAsync(() => reached100Signal.IsSet, TimeSpan.FromSeconds(5), TimeSpan.FromMilliseconds(50));
+        var processedAll = await WaitForConditionAsync(() => allProcessedSignal.IsSet, TimeSpan.FromSeconds(8), TimeSpan.FromMilliseconds(50));
+
+        await testFixture.Consumer.StopAsync(CancellationToken.None);
+
         // Assert
-        Assert.True(
-            await WaitForConditionAsync(() => reached100Signal.IsSet, TimeSpan.FromSeconds(3), TimeSpan.FromMilliseconds(20)),
-            "Did not process at least the first batch of 100 messages promptly.");
-
-        Assert.True(
-            await WaitForConditionAsync(() => allProcessedSignal.IsSet, TimeSpan.FromSeconds(8), TimeSpan.FromMilliseconds(50)),
-            "Remaining messages from the next batch were not processed within the expected window.");
-
-        await consumer.StopAsync(CancellationToken.None);
-
+        Assert.True(processedFirst100, "Did not process at least the first batch of 100 messages promptly.");
+        Assert.True(processedAll, "Remaining messages from the next batch were not processed within the expected window.");
         sendingServiceMock.Verify(e => e.SendAsync(It.IsAny<Core.Sending.Email>()), Times.Exactly(150));
+    }
+
+    /// <summary>
+    /// Creates and configures a <see cref="SendEmailQueueConsumer"/> instance from the provided <see cref="ServiceProvider"/>.
+    /// </summary>
+    /// <param name="serviceProvider">
+    /// The <see cref="ServiceProvider"/> containing the required services for the consumer.
+    /// </param>
+    /// <returns>
+    /// A fully constructed <see cref="SendEmailQueueConsumer"/> registered as an <see cref="IHostedService"/>.
+    /// </returns>
+    /// <exception cref="Xunit.Sdk.XunitException">
+    /// Thrown when the consumer instance cannot be resolved from the service provider.
+    /// </exception>
+    private static SendEmailQueueConsumer GetEmailSendingConsumer(ServiceProvider serviceProvider)
+    {
+        var emailSendingConsumer = serviceProvider.GetService(typeof(IHostedService)) as SendEmailQueueConsumer;
+
+        if (emailSendingConsumer == null)
+        {
+            Assert.Fail("Unable to create an instance of EmailSendingConsumer.");
+        }
+
+        return emailSendingConsumer;
     }
 
     /// <summary>
@@ -428,21 +435,45 @@ public class EmailSendingConsumerTests : IAsyncLifetime
     }
 
     /// <summary>
-    /// Creates and configures a <see cref="SendEmailQueueConsumer"/> instance for integration tests.
+    /// Creates a fully configured <see cref="EmailConsumerTestFixture"/> for integration testing of the email consumer.
+    /// </summary>
+    /// <param name="sendingService">
+    /// The <see cref="ISendingService"/> implementation to be used for email processing.
+    /// </param>
+    /// <param name="logger">
+    /// Optional <see cref="ILogger{T}"/> for <see cref="SendEmailQueueConsumer"/> to capture or control log output during testing.
+    /// </param>
+    /// <returns>
+    /// A configured <see cref="EmailConsumerTestFixture"/> containing:
+    /// <list type="bullet">
+    /// <item><description>A <see cref="SendEmailQueueConsumer"/> ready for testing</description></item>
+    /// <item><description>A <see cref="CommonProducer"/> for sending test messages to Kafka topics</description></item>
+    /// <item><description>A <see cref="ServiceProvider"/> with all required dependencies</description></item>
+    /// </list>
+    /// The fixture implements <see cref="IAsyncDisposable"/> and must be disposed properly to clean up resources.
+    /// </returns>
+    private EmailConsumerTestFixture CreateTestFixture(ISendingService sendingService, ILogger<SendEmailQueueConsumer>? logger = null)
+    {
+        var serviceProvider = CreateServiceProvider(sendingService, logger);
+        var consumer = GetEmailSendingConsumer(serviceProvider);
+        var producer = KafkaUtil.GetKafkaProducer(serviceProvider);
+
+        return new EmailConsumerTestFixture(producer, consumer, serviceProvider);
+    }
+
+    /// <summary>
+    /// Creates a configured <see cref="ServiceProvider"/> for integration tests.
     /// </summary>
     /// <param name="sendingService">
     /// The <see cref="ISendingService"/> to be injected into the consumer, typically a mocked implementation.
     /// </param>
     /// <param name="sendEmailQueueConsumerLogger">
-    /// Optional typed <see cref="ILogger{T}"/> for <see cref="SendEmailQueueConsumer"/> to capture or control logs emitted by the derived consumer.
+    /// Optional typed <see cref="ILogger{T}"/> for <see cref="SendEmailQueueConsumer"/> to capture or control logs emitted by the consumer.
     /// </param>
     /// <returns>
-    /// A fully constructed <see cref="SendEmailQueueConsumer"/> registered as an <see cref="IHostedService"/>.
+    /// A configured <see cref="ServiceProvider"/> with all necessary services registered for testing.
     /// </returns>
-    /// <exception cref="Xunit.Sdk.XunitException">
-    /// Thrown when the consumer instance cannot be resolved from the service provider.
-    /// </exception>
-    private SendEmailQueueConsumer GetEmailSendingConsumer(ISendingService sendingService, ILogger<SendEmailQueueConsumer>? sendEmailQueueConsumerLogger = null)
+    private ServiceProvider CreateServiceProvider(ISendingService sendingService, ILogger<SendEmailQueueConsumer>? sendEmailQueueConsumerLogger = null)
     {
         IServiceCollection services = new ServiceCollection()
            .AddLogging()
@@ -456,15 +487,6 @@ public class EmailSendingConsumerTests : IAsyncLifetime
             services.AddSingleton(sendEmailQueueConsumerLogger);
         }
 
-        _serviceProvider = services.BuildServiceProvider();
-
-        var emailSendingConsumer = _serviceProvider.GetService(typeof(IHostedService)) as SendEmailQueueConsumer;
-
-        if (emailSendingConsumer == null)
-        {
-            Assert.Fail("Unable to create an instance of EmailSendingConsumer.");
-        }
-
-        return emailSendingConsumer;
+        return services.BuildServiceProvider();
     }
 }
